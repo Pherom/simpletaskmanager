f6b0ce8ddf7ad89bc7836bd68a1588526d45cd25
Elad Moshe <pheromem@gmail.com>
2025-10-14 11:31:47 +0300

    Finished UserServiceIntegrationTest

--- src/test/java/com/pherom/simpletaskmanager/user/service/UserServiceIntegrationTest.java
+++ src/test/java/com/pherom/simpletaskmanager/user/service/UserServiceIntegrationTest.java
@@ -7,18 +7,17 @@ import com.pherom.simpletaskmanager.user.exception.EmailAlreadyExistsException;
 import com.pherom.simpletaskmanager.user.exception.UserNotFoundException;
 import com.pherom.simpletaskmanager.user.exception.UsernameAlreadyExistsException;
 import com.pherom.simpletaskmanager.user.repository.JpaUserRepository;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserServiceIntegrationTest {
 
     @Autowired
@@ -27,18 +26,19 @@ class UserServiceIntegrationTest {
     @Autowired
     private UserService service;
 
-    @BeforeAll
+    @BeforeEach
     void reset() {
-        repository.save(new User("Mark", "password123", "mark@gmail.com"));
-        repository.save(new User("Anna", "password246", "anna@gmail.com"));
+        repository.deleteAll();
     }
 
     @Test
     void updateUserByIdAndRetrieveIt() {
+        User existingUser = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+
         String username = "Shimon";
         String email = "shimon@gmail.com";
 
-        UserResponseDTO updateResponseDTO = service.updateById(1, new UserUpdateRequestDTO(username, email));
+        UserResponseDTO updateResponseDTO = service.updateById(existingUser.getId(), new UserUpdateRequestDTO(username, email));
         Optional<UserResponseDTO> findResponseDTO = service.findById(updateResponseDTO.id());
 
         assertTrue(findResponseDTO.isPresent());
@@ -55,16 +55,102 @@ class UserServiceIntegrationTest {
 
     @Test
     void updateUserWithAnAlreadyExistingUsername() {
-        String updatedUsername = "Anna";
-        UsernameAlreadyExistsException ex = assertThrows(UsernameAlreadyExistsException.class, () -> service.updateById(1, new UserUpdateRequestDTO(updatedUsername, null)));
-        assertTrue(ex.getMessage().contains(updatedUsername));
+        String existingUsername = "Anna";
+
+        User existingUser = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+        repository.save(new User(existingUsername, "password246", "anna@gmail.com"));
+
+        UsernameAlreadyExistsException ex = assertThrows(UsernameAlreadyExistsException.class, () -> service.updateById(existingUser.getId(), new UserUpdateRequestDTO(existingUsername, null)));
+        assertTrue(ex.getMessage().contains(existingUsername));
     }
 
     @Test
     void updateUserWithAnAlreadyExistingEmail() {
-        String updatedEmail = "anna@gmail.com";
-        EmailAlreadyExistsException ex = assertThrows(EmailAlreadyExistsException.class, () -> service.updateById(1, new UserUpdateRequestDTO("Shimon", updatedEmail)));
-        assertTrue(ex.getMessage().contains(updatedEmail));
+        String existingEmail = "anna@gmail.com";
+        User existingUser = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+        repository.save(new User("Anna", "password246", existingEmail));
+
+        EmailAlreadyExistsException ex = assertThrows(EmailAlreadyExistsException.class, () -> service.updateById(existingUser.getId(), new UserUpdateRequestDTO("Shimon", existingEmail)));
+        assertTrue(ex.getMessage().contains(existingEmail));
     }
 
+    @Test
+    void findUserByExistingId() {
+        User existingUser = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+
+        UserResponseDTO expected = new UserResponseDTO(existingUser.getId(), existingUser.getUsername(), existingUser.getEmail());
+        Optional<UserResponseDTO> responseDTO = service.findById(existingUser.getId());
+
+        assertTrue(responseDTO.isPresent());
+        assertEquals(expected, responseDTO.get());
+    }
+
+    @Test
+    void findUserByMissingId() {
+        assertTrue(service.findById(3).isEmpty());
+    }
+
+    @Test
+    void findAllUsersEmpty() {
+        List<UserResponseDTO> responseDTOS = service.findAll();
+        assertTrue(responseDTOS.isEmpty());
+    }
+
+    @Test
+    void findAllUsers() {
+        User existingUser1 = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+        User existingUser2 = repository.save(new User("Anna", "password246", "anna@gmail.com"));
+
+        List<UserResponseDTO> expected = List.of(
+                new UserResponseDTO(existingUser1.getId(), existingUser1.getUsername(), existingUser1.getEmail()),
+                new UserResponseDTO(existingUser2.getId(), existingUser2.getUsername(), existingUser2.getEmail())
+        );
+
+        List<UserResponseDTO> all = service.findAll();
+        assertEquals(expected, all);
+    }
+
+    @Test
+    void findByExistingUsername() {
+        User existingUser1 = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+        UserResponseDTO expected = new UserResponseDTO(existingUser1.getId(), existingUser1.getUsername(), existingUser1.getEmail());
+
+        Optional<UserResponseDTO> responseDTO = service.findByUsername(existingUser1.getUsername());
+
+        assertTrue(responseDTO.isPresent());
+        assertEquals(expected, responseDTO.get());
+    }
+
+    @Test
+    void findByMissingUsername() {
+        Optional<UserResponseDTO> responseDTO = service.findByUsername("something");
+        assertTrue(responseDTO.isEmpty());
+    }
+
+    @Test
+    void deleteByExistingId() {
+        User existingUser1 = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+
+        service.deleteById(existingUser1.getId());
+
+        Optional<User> foundUser = repository.findById(existingUser1.getId());
+        assertTrue(foundUser.isEmpty());
+    }
+
+    @Test
+    void deleteByMissingId() {
+        UserNotFoundException ex = assertThrows(UserNotFoundException.class, () -> service.deleteById(1));
+        assertTrue(ex.getMessage().contains("1"));
+    }
+
+    @Test
+    void deleteAll() {
+        User existingUser1 = repository.save(new User("Mark", "password123", "mark@gmail.com"));
+        User existingUser2 = repository.save(new User("Anna", "password246", "anna@gmail.com"));
+
+        service.deleteAll();
+
+        List<User> all = repository.findAll();
+        assertTrue(all.isEmpty());
+    }
 }
